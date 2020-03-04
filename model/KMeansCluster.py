a3de3a9f999dd28bb43a22b6a52e6d1099f02a04
andrewwhite5 <andrewwhite5@users.noreply.github.com>
2020-03-03 22:21:49 -0700

    Add train/test split

--- model/KMeansCluster.py
+++ model/KMeansCluster.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
 
 # Import
 df = pd.read_csv('track_master_df.csv')
@@ -19,7 +20,16 @@ labels = kmeans.labels_
 # Append cluster labels to dataframe
 new_series = pd.Series(labels)
 df_num['clusters'] = new_series.values
+# print(df_num)
 
 # Calculate each cluster's centroid
-centroids = df_num.groupby('clusters').mean()
-print(centroids)
+# centroids = df_num.groupby('clusters').mean()
+# print(centroids)
+
+# Specify X, y
+X = df_num.drop(columns='clusters')
+y = df_num['clusters']
+
+# Train/test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, test_size=0.33, random_state=42)
