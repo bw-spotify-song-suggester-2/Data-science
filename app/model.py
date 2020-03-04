88e9db7b70277945345ae084e9f952e56ef6dab5
ezorigo <bzorigo@gmail.com>
2020-03-03 22:30:29 -0500

    minor update

--- app/model.py
+++ app/model.py
@@ -12,11 +12,26 @@ import numpy as np
 ##BUILD PREDICTOR##
 ###################
 
-loaded_model = pickle.load(open('model\knnpickled', 'rb'))
-
 class Predictor():
     def __init__(self, model=None):
-        self.model = loaded_model
+        self.model = load_file('model')
 
     def predict(self, user_input=None, size=10):
         pass
+
+def get_abs_path(filename, **kwargs):
+    if os.path.isfile(os.path.abspath(filename)):
+        return os.path.abspath(filename)
+    else:
+        return os.path.join(
+            os.getcwd(), 'app/model/'+filename,
+        )
+        
+def load_file(file_key):
+    with open(get_abs_path(params[file_key]), 'rb') as f:
+        opened = pickle.load(f)
+    return opened
+
+params = {
+    'model': 'knnpickled'
+}
